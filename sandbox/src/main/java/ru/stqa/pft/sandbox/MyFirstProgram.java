08d0e93446e414b7547f9e3bc2bbef0b5759feac
thedsv <thedsv@hotmail.com>
2017-08-16 01:26:57 +0300

    Изменено название класса
    GitHub не видел разницы между myFirstProgram и MyFirstProgram.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram1 {
+public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
