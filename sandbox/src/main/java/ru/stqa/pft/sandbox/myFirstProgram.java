21cbf1ce484af99d575eca3f60090444a862c698
thedsv <thedsv@hotmail.com>
2017-08-15 22:19:56 +0300

    Изменено название класса и форматирование кода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
-public class myFirstProgram {
-	public static void main(String[] args) {		
-System.out.println("Hello, world!");
-	}
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
