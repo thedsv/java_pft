4a6c0fd02866938943ec044ee317177fe2f30cdf
thedsv <thedsv@hotmail.com>
2017-09-01 00:32:48 +0300

    Отключен таймаут ожидания

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -30,7 +30,7 @@ public class ApplicationManager {
     } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     sessionHelper = new SessionHelper(wd);
     navigationHelper = new NavigationHelper(wd);
