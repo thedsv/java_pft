c3ba540a725467d0121731048a2029977c28e1db
thedsv <thedsv@hotmail.com>
2017-10-17 00:38:03 +0300

    Тесты soap запросов к MantisBT

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.mantis.tests;
+
+import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
+import ru.stqa.pft.mantis.model.Project;
+
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class SoapTests extends TestBase{
+
+  @Test
+  public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+    Set<Project> projects = app.soap().getProjects();
+    System.out.println(projects.size());
+    for (Project project : projects) {
+      System.out.println(project.getName());
+    }
+  }
+
+  @Test
+  public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+    Set<Project> projects = app.soap().getProjects();
+    Issue issue = new Issue().withSummary("Test issue")
+            .withDescription("Test issue description").withProject(projects.iterator().next());
+    Issue created = app.soap().addIssue(issue);
+    assertEquals(issue.getSummary(), created.getSummary());
+  }
+
+}
