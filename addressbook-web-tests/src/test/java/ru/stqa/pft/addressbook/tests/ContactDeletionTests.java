93c28547af5082badc40d6892cf522b15677c9fa
thedsv <thedsv@hotmail.com>
2017-10-18 02:15:47 +0300

    Добавлена проверка статуса дефекта в Bugify перед запуском тестов удаления контактов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -5,6 +5,9 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import javax.xml.rpc.ServiceException;
+import java.io.IOException;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -12,7 +15,8 @@ import static org.testng.Assert.assertEquals;
 public class ContactDeletionTests extends TestBase {
 
   @BeforeMethod
-  public void ensurePreconditions() {
+  public void ensurePreconditions() throws IOException, ServiceException {
+    skipIfNotFixed("bugify", 274);
     if (app.db().contacts().size() == 0) {
       app.goTo().homePage();
       app.contact().create(new ContactData().withFirstName("bornToBeDeleted"));
