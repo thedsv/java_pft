f7ee8b4aa9bd893b6d00fff3f17922bd9673bc63
thedsv <thedsv@hotmail.com>
2017-10-18 22:34:08 +0300

    Испорчен тест для проверки Jenkins

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -8,7 +8,8 @@ public class EquationTests {
   @Test
   public void test0() {
     Equation e = new Equation(1,1,1);
-    Assert.assertEquals(e.rootNumber(), 0);
+//    Assert.assertEquals(e.rootNumber(), 0);
+    Assert.assertEquals(e.rootNumber(), 1);
   }
 
   @Test
